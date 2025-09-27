8b602255609d7b7d6c5c2e727a04019a8a547fbc
lucasabreuzip <lucasabreu.abr@gmail.com>
2025-09-27 19:06:40 -0300

    Adicionado nova URL CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     ALLOWED_ORIGINS: str = (
         "http://localhost:3000,http://localhost:5173,http://localhost:5174,http://localhost:5175,"
         "http://127.0.0.1:3000,http://127.0.0.1:5173,http://127.0.0.1:5174,http://127.0.0.1:5175,"
-        "http://localhost:8080,http://127.0.0.1:8080,http://127.0.0.1:8000"
+        "http://localhost:8080,http://127.0.0.1:8080,http://127.0.0.1:8000,https://site-unireservas.onrender.com/"
     )
 
     # JWT -- REMOVIDO (usado para tests)
